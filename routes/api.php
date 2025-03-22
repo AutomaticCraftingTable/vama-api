da6983fceef243b8d823a3aacd897aed87e7412e
Adam Kasprzyk <adam.kasprzyk@studenci.collegiumwitelona.pl>
2025-03-22 16:06:26 +0100

    log-in

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::middleware("auth:sanctum")->get("/user", fn (Request $request): JsonResponse => new JsonResponse($request->user()));
 
-Route::get('/hello', function () {
-    return response()->json(['message' => 'Hello, World!']);
-});
+Route::prefix('auth')->group(function () {
+Route::post('login', [AuthController::class, 'login']);
+
+Route::post('register', [AuthController::class, 'register']);
+
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});;
