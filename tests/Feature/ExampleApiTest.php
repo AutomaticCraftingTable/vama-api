fbebf220e67bfb61bfbe69b4ae99008a88fed03a
Adam Kasprzyk <adam.kasprzyk@studenci.collegiumwitelona.pl>
2025-04-03 19:17:44 +0200

    test

---
+++ tests/Feature/ExampleApiTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleApiTest extends TestCase
+{
+    public function testHelloWorldEndpoint()
+    {
+        $this->assertTrue(true);
+    }
+}
