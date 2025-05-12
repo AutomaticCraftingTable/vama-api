6b695f8f56f33bae33a019bec48b0c8d2b342a16
Adam Kasprzyk <adam.kasprzyk@studenci.collegiumwitelona.pl>
2025-05-12 21:39:26 +0200

    merge fix

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AuthController;
 
 Route::middleware("auth:sanctum")->get("/user", fn (Request $request): JsonResponse => new JsonResponse($request->user()));
 
@@ -16,13 +15,4 @@ Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
 
     Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-
-    Route::post('register', [AuthController::class, 'register']);
-
-    Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
-
-
-;
